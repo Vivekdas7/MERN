64585403269aa5647d114b4ccd161b08a89ab9bf
VIVEK DAS <123769894+Vivekdas7@users.noreply.github.com>
2024-01-12 18:33:32 +0530

    day2

--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,23 +1,24 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
-        statuscode,
-        message="something went wrong",
-        errors=[],
-        statck=""
-
+        statusCode,
+        message= "Something went wrong",
+        errors = [],
+        stack = ""
     ){
         super(message)
-        this.statuscode=statuscode
-        this.data=null
-        this.message=message
-        this.sucess=false;
-        this.errors=errors
-        if(statck){
-            this.stack=statck
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
         }
-        else{
-            Error.captureStackTrace(this,this.constructor)
-        }
+
     }
 }
-export{ApiError}
+
+export {ApiError}
